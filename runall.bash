65928c36fa07f70ebb57ac3ffbcf96b4198c6bac
Mark <mark3ting.sci3nc3s@gmail.com>
2018-01-08 11:31:11 +0100

    allow non-zero exit code for runall.bash

--- runall.bash
+++ runall.bash
@@ -1,9 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 for i in *.ipynb;do
     echo "Running $i"
     j=$(basename -s .ipynb $i)
     k=$j.py
     ipython3 nbconvert --to python $i &> $i.log
+    set +e
     python3 $k &> $k.log
+    set -e
 done
 
